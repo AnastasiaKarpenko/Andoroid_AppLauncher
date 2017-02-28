052406580692169389925787065028a0d7aa524d
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-28 10:19:31 +0200

    Querying the PackageManager and Sorting alphabetically the lables of the apps on the phone

--- app/src/main/java/ws/tilda/anastasia/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/ws/tilda/anastasia/nerdlauncher/NerdLauncherFragment.java
@@ -1,15 +1,26 @@
 package ws.tilda.anastasia.nerdlauncher;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class NerdLauncherFragment extends Fragment{
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class NerdLauncherFragment extends Fragment {
+    public static final String TAG = "NerdLauncherFragment";
+
     private RecyclerView mRecyclerView;
+
     public static NerdLauncherFragment newInstance() {
         return new NerdLauncherFragment();
     }
@@ -19,6 +30,31 @@ public class NerdLauncherFragment extends Fragment{
         View v = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
         mRecyclerView = (RecyclerView) v.findViewById(R.id.app_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        setupAdapter();
+
         return v;
     }
+
+    private void setupAdapter() {
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo a, ResolveInfo b) {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        a.loadLabel(pm).toString(),
+                        b.loadLabel(pm).toString());
+            }
+        });
+
+        Log.i(TAG, "Found " + activities.size() + " activities.");
+
+    }
+
+
 }
